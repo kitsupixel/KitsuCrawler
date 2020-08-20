5f0d8b696e23e4a0b8982e52771a9de0d7c106e7
Nelson Costa <nelson.costa@sincelo.pt>
2020-08-20 16:48:14 +0100

    Created a test crawl

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"KitsuCrawler/core"
+	"fmt"
+	"time"
+)
+
 func main() {
-	
+	start := time.Now()
+
+	userAgent := "KitsuBot"
+	websiteToCrawl := "https://www.mbit.pt"
+
+	crawler, err := core.NewCrawler(websiteToCrawl, userAgent)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	crawler.Start()
+
+	elapsed := time.Since(start)
+
+	fmt.Printf("\n\nCrawler took %s", elapsed)
 }
